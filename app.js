ca91ce0da57d9eb62213986153ee3af8a4a985f7
SindhujaVilipala <svilipala@gmail.com>
2022-04-06 16:23:59 -0500

    editing

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');  
-var RestaurantRouter = require('./routes/Restaurant');  //Restaurant
+var RestaurantRouter = require('./routes/Restaurant');
+var selectorRouter = require('./routes/selector'); 
+var addmodsRouter = require('./routes/addmods'); //Restaurant
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Restaurant', RestaurantRouter);
+app.use('/selector', selectorRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
