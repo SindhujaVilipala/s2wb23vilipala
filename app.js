1026c68d604e1ffec7b4bd8d81dee37093120731
SindhujaVilipala <svilipala@gmail.com>
2022-04-06 14:06:55 -0500

    upd

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');  
 var RestaurantRouter = require('./routes/Restaurant');  //Restaurant
 
-
 var app = express();
 
 // view engine setup
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Restaurant', RestaurantRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
