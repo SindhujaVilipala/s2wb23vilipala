01562769052f35304cd58ef99604294f17da5705
SindhujaVilipala <svilipala@gmail.com>
2022-04-06 14:01:53 -0500

    edi

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var RestaurantRouter = require('./routes/Restaurant');
+var usersRouter = require('./routes/users');  
+var RestaurantRouter = require('./routes/Restaurant');  //Restaurant
+
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Restaurant', RestaurantRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
